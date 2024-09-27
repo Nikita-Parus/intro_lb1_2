6f13ee2c0645ae67b55e76224d34e63471eea7a8
Nikita-Parus <cheems.nz@gmail.com>
2024-09-28 00:43:41 +0300

    Добавьте файлы проекта.

---
+++ intro_lb1_2/intro_lb1_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string isodd(int num) {
+    return (num % 2 == 0 ? " парне" : " непарне");
+}
+
+int main()
+{
+    setlocale(0, ".1251");
+    int a, b, c;
+    cout << "Введiть цiле число: ";
+    cin >> a;
+    cout << "Залишок вiд дiлення на 7: " << a % 7 << endl;
+
+    cin >> b >> c;
+
+    cout << ((b % c == 0) ? "Числа дiляться без залишку" : "Числа не дiляться без залишку") << endl;
+
+    cout << "Число " << b << isodd(b) << endl;
+    cout << "Число " << c << isodd(c) << endl;
+}
